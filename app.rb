73fa434b5ba1437cee79bb560de9fd7ffb823e35
Koren <koren.hmel@gmail.com>
2016-11-18 20:41:57 +0200

    update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+  # class method
   def self.say_smth
     puts 'say hello Logger!!!'
+  end
+  # instance method
+  def log_smth wat
+    @f.puts wat
   end
 end
 
 Logger.say_smth
+
+logger = Logger.new
+logger.log_smth 'hey!'
