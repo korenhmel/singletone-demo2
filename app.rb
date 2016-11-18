95286f0a90a46b6f542ee4dde5864b87ffaa316e
Koren <koren.hmel@gmail.com>
2016-11-18 21:41:41 +0200

    refactoring: extract logger to separate file

--- app.rb
+++ app.rb
@@ -1,32 +1,8 @@
-class Logger
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
+require './logger'
 
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-    @@x
-  end
-
-  # class method
-  def self.say_smth
-    puts 'say hello Logger!!!'
-  end
-
-  # instance method
-  def log_smth wat
-    @f.puts wat
-  end
-end
-
-Logger.say_smth
 Logger.instance.log_smth 'bla bla 1'
 Logger.instance.log_smth 'bla bla 2'
 
-logger = Logger.new
-logger.log_smth 'hey'
+# logger = Logger.new
+# logger.log_smth 'hey'
 
